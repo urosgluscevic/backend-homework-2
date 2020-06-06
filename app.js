50880399f0b753062d2a68bbb44065ad4b4678e5
MiodragKruscic <miokruscic@gmail.com>
2020-06-06 12:59:23 +0200

    sfasfa

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const { json, urlencoded } = require('body-parser');
 
+
+const connect = require("./helpers").connect;
+const Books = require("./controllers.js");
+
+
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
+
+app.get("/allBooks", async (req,res)=>{
+    const allBooks = await Books.findAllBooks();
+    res.status(200).json(allBooks);
+})
+
